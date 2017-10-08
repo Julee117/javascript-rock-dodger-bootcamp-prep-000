b187f85879d43b1777a06454a74ee9eb7159738a
Julianne Kim <juliannne@gmail.com>
2017-10-08 21:16:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -136,6 +136,11 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   if (e.which === 37) {
+     return moveDodgerLeft();
+   } else if (e.which === 39) {
+     return moveDodgerRight();
+   }
 }
 
 function moveDodgerLeft() {
