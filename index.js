4660544a61d10380f7fd7e1c1df76585cce84852
Julianne Kim <juliannne@gmail.com>
2017-10-08 21:20:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -138,8 +138,12 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if (e.which === LEFT_ARROW) {
+     e.preventDefault();
+     e.stopPropagation();
      return moveDodgerLeft();
    } else if (e.which === RIGHT_ARROW) {
+     e.preventDefault();
+     e.stopPropagation();
      return moveDodgerRight();
    }
 }
