5b8ac6a3efbb3be2eb1aaebba992579b1e7ecf98
Julianne Kim <juliannne@gmail.com>
2017-10-08 22:05:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ function moveDodgerRight() {
    */
    window.requestAnimationFrame(function(){
      const right = positionToInteger(dodger.style.right);
-     if (right < 400) {
+     if (right < 360) {
        dodger.style.right = `${right + 4}px`;
      }
    })
